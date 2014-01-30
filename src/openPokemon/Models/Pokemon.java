6ca7e499e7604eec37bc63075d0191f75984dcc4
Chris Seto <Chriskseto@gmail.com>
2014-01-29 22:39:49 -0500

    Added some properties.

--- src/openPokemon/Models/Pokemon.java
+++ src/openPokemon/Models/Pokemon.java
@@ -1,14 +1,26 @@
-// [SublimeLinter javac-lint:all,-deprecation]
 package src.openPokemon.Models;
 
+import java.util.Map;
+
+import src.openPokemon.Enums.Nature;
 import src.openPokemon.Enums.Type;
 
 public abstract class Pokemon {
-    private int currentHealth, health;
-    private Type types[];
+	public static String Name, Classification;
+	public static int PokedexNumber,StepsToHatch;
+	public static Map<Integer,Move> MovesByLevelUp;
+	public static Move[] LearnableMoves;
+	
+	
+	
+    private int currentHealth;
+    private Type[] types;
     private boolean isWild,hasBeenCaught;
-    private int level;
-
+    private int level,health,speed,attack,specialAttack,defence,specialDefence;
+    private Nature nature;
+    private Move[] moves;
+    
+    
     public String serialize()
     {
         //TODO Finish me
