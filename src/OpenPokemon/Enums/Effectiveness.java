7b83452293acd8841e69357d92da363480a8eb83
Djvacto <Djvacto@gmail.com>
2014-01-31 10:00:06 -0500

    Update Effectiveness.java
    I added the 4x Effectiveness for when a dual type Pokemon is hit with a move both types are weak to, like Gyarados and Electric or Charizard and Rock

--- src/OpenPokemon/Enums/Effectiveness.java
+++ src/OpenPokemon/Enums/Effectiveness.java
@@ -1,7 +1,8 @@
 package src.openPokemon.Enums;
 
 public enum Effectiveness{
-    SUPEREFFECTIVE (2),
+    DOUBLESUPEREFFECTIVE (4), //Not sure what to call it, but if there is a double type weakness, for example, Gyarados (W/F)
+    SUPEREFFECTIVE (2),       //Versus an electric type mov, the move does 4 times damage
     NORMAL (1),
     NOTVERYEFFECTIVE (.5f),
     NOEFFECT (0);
